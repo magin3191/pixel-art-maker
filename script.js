440c9dc2627d7596da8bcfe7d824bb40e2818ddd
Matt <magin3191@gmail.com>
2017-11-11 12:05:55 -0500

    updates

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function() {
 
-  for (var i=0;i<464;i++){
-    var canvas=document.createElement('div');
-    canvas.className='canvas';
-    canvas.style.height='20px';
-    canvas.style.width='20px';
-    canvas.style.float='left';
+  for (var i = 0; i < 464; i++) {
+    var canvas = document.createElement('div');
+    canvas.className = 'canvas';
+    canvas.style.height = '20px';
+    canvas.style.width = '20px';
+    canvas.style.float = 'left';
 
 
 
@@ -13,64 +13,60 @@ document.addEventListener('DOMContentLoaded',function(){
 
 
 
-    var container=document.getElementsByClassName('container')[0];
+    var container = document.getElementsByClassName('container')[0];
 
 
     container.appendChild(canvas);
 
 
 
-  }//end of for loop.
+  } //end of for loop.
 
 
-var color='';
+  var color = '';
 
-var colorWheelContainer = document.getElementById('colorWheelContainer');
+  var colorWheelContainer = document.getElementById('colorWheelContainer');
 
-console.log(colorWheelContainer)
+  console.log(colorWheelContainer)
 
-colorWheelContainer.addEventListener('click',function(event){
-color = event.target.style.backgroundColor;
-
-
-})
+  colorWheelContainer.addEventListener('click', function(event) {
+    color = event.target.style.backgroundColor;
 
 
-container.addEventListener('click', function(event){  //prevents filling color through whole container
-
-if (event.target.className !== "container") {
-    event.target.style.backgroundColor = color
-}
+  })
 
 
+  container.addEventListener('click', function(event) { //prevents filling color through whole container
 
-
-})
-
-
-var resetButton = document.getElementById('reset'); //reset button loops over all divs
-resetButton.addEventListener('click',reset)
-
-function reset(){
-  for(var j=0;j<464;j++){
-    document.getElementsByClassName('canvas')[j].style.backgroundColor='white';
-
-
-  }
-
-
-}
-
-
-
-
-
-
-
-
-
+    if (event.target.className !== "container") {
+      event.target.style.backgroundColor = color
+    }
 
 
 
 
   })
+
+
+  var resetButton = document.getElementById('reset'); //reset button loops over all divs
+  resetButton.addEventListener('click', reset)
+
+  function reset() {
+    for (var j = 0; j < 464; j++) {
+      document.getElementsByClassName('canvas')[j].style.backgroundColor = 'white';
+
+
+    }
+
+
+  }
+
+
+
+
+
+
+
+
+
+})
